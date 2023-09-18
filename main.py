33dd624bea5e3614cd38e53fa1e4d3352f2996b7
Rene Probst <rene.probst@bzz.ch>
2023-09-18 22:45:07 +0200

    test cases revised

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from builtins import int
 
+class ListIndexException(Exception):
+    def __init__(self, index):
+        super().__init__(f'Fehler: Ungültiger Index: {index}')
+
+class ListRangeException(Exception):
+    pass
+
+
+class ValueRangeException(Exception):
+
+    def __init__(self, grade: float):
+        super().__init__(f'Fehler: Der Notenwert muss im Bereich 1.0 bis 6.0 liegen. Er beträgt jedoch {grade}.')
+
 
 class GradeList:
     def __init__(self):
@@ -7,40 +20,39 @@ class GradeList:
         self._grades = []
 
     def add_grade(self, grade: float) -> None:
-        if grade < 1.0 or grade > 6.0:
-            raise GradeRangeError(grade)
-        elements = self.get_current_grade_count()
-        if elements >= self._MAX_GRADE_COUNT:
-            raise Overflow('Fehler: Zu viele Werte eingegeben\n')
-        self._grades.append(grade)
+        if 1.0 <= grade <= 6.0:
+            elements = self.current_grade_count
+            if elements >= self._MAX_GRADE_COUNT:
+                raise ListRangeException('Fehler: Zu viele Werte eingegeben\n')
+            print(f'zufügen von Note mit {grade}' )
+            self._grades.append(grade)
+        else:
+            raise ValueRangeException(grade)
 
-    def get_max_grade_count(self) -> int:
+
+    @property
+    def max_grade_count(self) -> int:
         return self._MAX_GRADE_COUNT
 
-    def get_current_grade_count(self) -> int:
+    @property
+    def current_grade_count(self) -> int:
         return len(self._grades)
 
-    def get_grade(self, index: int) -> float:
-        return self._grades[index]
+    def take_grade(self, index: int) -> float:
+        if 0 <= index < self.current_grade_count:
+          return self._grades[index]
+        else:
+            raise ListIndexException(index)
 
     def remove_grade(self, index: int) -> None:
         self._grades.pop(index)
 
     def print(self) -> None:
-        r = range(self.get_current_grade_count())
+        r = range(self.current_grade_count)
         for i in r:
             print(f"{i + 1}. Note: {self._grades[i]}")
 
 
-class Overflow(RuntimeError):
-    pass
-
-
-class GradeRangeError(RuntimeError):
-
-    def __init__(self, grade: int):
-        message = f'Fehler: Der Notenwert muss im Bereich 1.0 bis 6.0 liegen. Er beträgt jedoch {grade}.'
-        super().__init__(self, message)
 
 
 if __name__ == '__main__':
@@ -48,15 +60,16 @@ if __name__ == '__main__':
     try:
         demo.add_grade(4.5)
         demo.add_grade(5.0)
+        demo.add_grade(6.0)
+        demo.add_grade(2.5)
         demo.add_grade(3.5)
-        demo.add_grade(4.0)
-        demo.add_grade(0.5)
-        # und einen nächsten Wert einfügen, der zu einem Overflow führen wird.
-        demo.add_grade(3.5)
-    except GradeRangeError as ge:
+        demo.add_grade(4.5)
+    except ListRangeException as ge:
         print(ge)
-    except Overflow as of:
-        print(of)
+    try:
+        demo.add_grade(8.0)
+    except ValueRangeException as ve:
+        print(ve)
     demo.print()
 
     print('\nLösche Wert an 2. Stelle')
@@ -65,11 +78,11 @@ if __name__ == '__main__':
 
     # und nun einen Wert an einer Stelle lesen, die es nicht gibt
     try:
-        print(f'\n8. Note = {demo.get_grade(8)}')
-    except IndexError:
-        print(f'Falscher Index; gültig sind Werte von 0 bis {demo.get_max_grade_count() - 1}')
+        print(f'\n8. Note = {demo.take_grade(8)}')
+    except ListIndexException as le:
+        print(le)
 
-    print(f'\nListe umfasst zur Zeit {demo.get_current_grade_count()} Noten')
-    print(f'Note an 3. Stelle ist {demo.get_grade(2)}')
-    print(f'Grösse der Liste beträgt {demo.get_max_grade_count()}\n')
+    print(f'\nListe umfasst zur Zeit {demo.current_grade_count} Noten')
+    print(f'Note an 3. Stelle ist {demo.take_grade(2)}')
+    print(f'Grösse der Liste beträgt {demo.max_grade_count}\n')
     demo.print()
--- main_test.py
+++ main_test.py
@@ -1,10 +1,36 @@
-from main import GradeList
+import pytest
 
-def test_main():
-    demo = GradeList()
-    demo.add_grade(4.5)
-    demo.add_grade(5.0)
-    demo.add_grade(3.5)
-    demo.add_grade(4.0)
-    demo.add_grade(4.5)
-    demo.add_grade(5.5)
+from main import GradeList, ListIndexException, ListRangeException, ValueRangeException
+
+class TestGrade:
+
+    @pytest.fixture
+    def grades(self):
+        gl = GradeList()
+        gl.add_grade(4.5)
+        return gl
+
+
+    def test_list_index_ok(self, grades):
+        assert grades.take_grade(0) == 4.5
+
+    def test_list_index_exception(self, grades):
+        with pytest.raises(ListIndexException):
+            grades.take_grade(4)
+
+    def test_value_range_ok(self, grades):
+        grades.add_grade(5.0)
+        assert grades.take_grade(1) == 5.0
+
+    def test_value_range_low(self, grades):
+        with pytest.raises(ValueRangeException):
+            grades.add_grade(0.5)
+
+    def test_value_range_high(self, grades):
+        with pytest.raises(ValueRangeException):
+            grades.add_grade(6.5)
+
+    def test_numer_too_large(self, grades):
+        with pytest.raises(ListRangeException):
+            for cnt in range(6):
+                grades.add_grade(5.0)
